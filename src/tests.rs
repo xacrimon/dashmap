36ed364bea33c6e6ccf33e66b5c574af4606762b
Acrimon <joel.wejdenstal@gmail.com>
2019-09-02 23:14:57 +0200

    Added iterator length test.

--- src/tests.rs
+++ src/tests.rs
@@ -13,3 +13,11 @@ fn insert_and_remove() {
     map.query().insert(19, 420).sync().exec();
     assert_eq!(map.query().remove(&19).sync().exec(), Some((19, 420)));
 }
+
+#[test]
+fn insert_iter_count() {
+    let map: DashMap<i32, i32> = DashMap::new();
+    map.query().insert(19, 420).sync().exec();
+    map.query().insert(13, 420).sync().exec();
+    assert_eq!(map.query().iter().exec().count(), 2);
+}
