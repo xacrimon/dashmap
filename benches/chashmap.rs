c48a0542640cde140c41f6f3b59cc4588ab15953
Acrimon <joel.wejdenstal@gmail.com>
2019-11-19 19:57:22 +0100

    chashmap bench fix.

--- benches/chashmap.rs
+++ benches/chashmap.rs
@@ -1,6 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId, Throughput};
 use rayon::prelude::*;
 use chashmap::CHashMap;
+use std::cmp;
 
 const ITER: u64 = 8 * 1024;
 
@@ -15,7 +16,7 @@ fn task_insert_chashmap_u64_u64() -> CHashMap<u64, u64> {
 fn insert_chashmap_u64_u64(c: &mut Criterion) {
     let mut group = c.benchmark_group("insert_chashmap_u64_u64");
     group.throughput(Throughput::Elements(ITER as u64));
-    let max = num_cpus::get();
+    let max = cmp::max(num_cpus::get(), 24);
 
     for threads in 1..=max {
         group.bench_with_input(BenchmarkId::from_parameter(threads), &threads, |b, &threads| {
