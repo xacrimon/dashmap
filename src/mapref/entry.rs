194b56456669b597c90e8b6df17bbbc30f75030c
Ethan Smith <ethan@ethanhs.me>
2020-01-14 19:07:45 -0800

    Make DashMap generic to the hasher

--- src/mapref/entry.rs
+++ src/mapref/entry.rs
@@ -3,16 +3,16 @@ use crate::util;
 use crate::util::SharedValue;
 use crate::HashMap;
 use parking_lot::RwLockWriteGuard;
-use std::hash::Hash;
+use std::hash::{BuildHasher, Hash};
 use std::mem;
 use std::ptr;
 
-pub enum Entry<'a, K: Eq + Hash, V> {
-    Occupied(OccupiedEntry<'a, K, V>),
-    Vacant(VacantEntry<'a, K, V>),
+pub enum Entry<'a, K: Eq + Hash, V, S: BuildHasher> {
+    Occupied(OccupiedEntry<'a, K, V, S>),
+    Vacant(VacantEntry<'a, K, V, S>),
 }
 
-impl<'a, K: Eq + Hash, V> Entry<'a, K, V> {
+impl<'a, K: Eq + Hash, V, S: BuildHasher> Entry<'a, K, V, S> {
     /// Apply a function to the stored value if it exists.
     #[inline]
     pub fn and_modify(self, f: impl FnOnce(&mut V)) -> Self {
@@ -38,7 +38,7 @@ impl<'a, K: Eq + Hash, V> Entry<'a, K, V> {
     /// Return a mutable reference to the element if it exists,
     /// otherwise insert the default and return a mutable reference to that.
     #[inline]
-    pub fn or_default(self) -> RefMut<'a, K, V>
+    pub fn or_default(self) -> RefMut<'a, K, V, S>
     where
         V: Default,
     {
@@ -51,7 +51,7 @@ impl<'a, K: Eq + Hash, V> Entry<'a, K, V> {
     /// Return a mutable reference to the element if it exists,
     /// otherwise a provided value and return a mutable reference to that.
     #[inline]
-    pub fn or_insert(self, value: V) -> RefMut<'a, K, V> {
+    pub fn or_insert(self, value: V) -> RefMut<'a, K, V, S> {
         match self {
             Entry::Occupied(entry) => entry.into_ref(),
             Entry::Vacant(entry) => entry.insert(value),
@@ -61,7 +61,7 @@ impl<'a, K: Eq + Hash, V> Entry<'a, K, V> {
     /// Return a mutable reference to the element if it exists,
     /// otherwise insert the result of a provided function and return a mutable reference to that.
     #[inline]
-    pub fn or_insert_with(self, value: impl FnOnce() -> V) -> RefMut<'a, K, V> {
+    pub fn or_insert_with(self, value: impl FnOnce() -> V) -> RefMut<'a, K, V, S> {
         match self {
             Entry::Occupied(entry) => entry.into_ref(),
             Entry::Vacant(entry) => entry.insert(value()),
@@ -69,22 +69,25 @@ impl<'a, K: Eq + Hash, V> Entry<'a, K, V> {
     }
 }
 
-pub struct VacantEntry<'a, K: Eq + Hash, V> {
-    shard: RwLockWriteGuard<'a, HashMap<K, V>>,
+pub struct VacantEntry<'a, K: Eq + Hash, V, S> {
+    shard: RwLockWriteGuard<'a, HashMap<K, V, S>>,
     key: K,
 }
 
-unsafe impl<'a, K: Eq + Hash + Send, V: Send> Send for VacantEntry<'a, K, V> {}
-unsafe impl<'a, K: Eq + Hash + Send + Sync, V: Send + Sync> Sync for VacantEntry<'a, K, V> {}
+unsafe impl<'a, K: Eq + Hash + Send, V: Send, S: BuildHasher> Send for VacantEntry<'a, K, V, S> {}
+unsafe impl<'a, K: Eq + Hash + Send + Sync, V: Send + Sync, S: BuildHasher> Sync
+    for VacantEntry<'a, K, V, S>
+{
+}
 
-impl<'a, K: Eq + Hash, V> VacantEntry<'a, K, V> {
+impl<'a, K: Eq + Hash, V, S: BuildHasher> VacantEntry<'a, K, V, S> {
     #[inline]
-    pub(crate) fn new(shard: RwLockWriteGuard<'a, HashMap<K, V>>, key: K) -> Self {
+    pub(crate) fn new(shard: RwLockWriteGuard<'a, HashMap<K, V, S>>, key: K) -> Self {
         Self { shard, key }
     }
 
     #[inline]
-    pub fn insert(mut self, value: V) -> RefMut<'a, K, V> {
+    pub fn insert(mut self, value: V) -> RefMut<'a, K, V, S> {
         unsafe {
             let c: K = ptr::read(&self.key);
             self.shard.insert(self.key, SharedValue::new(value));
@@ -108,19 +111,22 @@ impl<'a, K: Eq + Hash, V> VacantEntry<'a, K, V> {
     }
 }
 
-pub struct OccupiedEntry<'a, K: Eq + Hash, V> {
-    shard: RwLockWriteGuard<'a, HashMap<K, V>>,
+pub struct OccupiedEntry<'a, K: Eq + Hash, V, S: BuildHasher> {
+    shard: RwLockWriteGuard<'a, HashMap<K, V, S>>,
     elem: (&'a K, &'a mut V),
     key: Option<K>,
 }
 
-unsafe impl<'a, K: Eq + Hash + Send, V: Send> Send for OccupiedEntry<'a, K, V> {}
-unsafe impl<'a, K: Eq + Hash + Send + Sync, V: Send + Sync> Sync for OccupiedEntry<'a, K, V> {}
+unsafe impl<'a, K: Eq + Hash + Send, V: Send, S: BuildHasher> Send for OccupiedEntry<'a, K, V, S> {}
+unsafe impl<'a, K: Eq + Hash + Send + Sync, V: Send + Sync, S: BuildHasher> Sync
+    for OccupiedEntry<'a, K, V, S>
+{
+}
 
-impl<'a, K: Eq + Hash, V> OccupiedEntry<'a, K, V> {
+impl<'a, K: Eq + Hash, V, S: BuildHasher> OccupiedEntry<'a, K, V, S> {
     #[inline]
     pub(crate) fn new(
-        shard: RwLockWriteGuard<'a, HashMap<K, V>>,
+        shard: RwLockWriteGuard<'a, HashMap<K, V, S>>,
         key: Option<K>,
         elem: (&'a K, &'a mut V),
     ) -> Self {
@@ -143,7 +149,7 @@ impl<'a, K: Eq + Hash, V> OccupiedEntry<'a, K, V> {
     }
 
     #[inline]
-    pub fn into_ref(self) -> RefMut<'a, K, V> {
+    pub fn into_ref(self) -> RefMut<'a, K, V, S> {
         RefMut::new(self.shard, self.elem.0, self.elem.1)
     }
 
