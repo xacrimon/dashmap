8f1911e4f6e9f43e3d76e40ece42e8192e105161
Acrimon <joel.wejdenstal@gmail.com>
2020-01-24 16:54:59 +0100

    Added get and get_mut methods to BucketArray.

--- src/table.rs
+++ src/table.rs
@@ -1,13 +1,15 @@
 use super::element::*;
-use crossbeam_epoch::{Atomic, Guard, Owned, Shared};
-use std::hash::{Hash, BuildHasher};
+use crossbeam_epoch::{pin, Atomic, Guard, Owned, Shared};
+use std::borrow::Borrow;
+use std::hash::{BuildHasher, Hash, Hasher};
+use std::iter;
+use std::mem;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
-use std::mem;
-use std::iter;
 
 const PTR_SIZE_BITS: usize = mem::size_of::<usize>() * 8;
 const REDIRECT_TAG: usize = 5;
+const TOMBSTONE_TAG: usize = 7;
 
 fn make_shift(x: usize) -> usize {
     debug_assert!(x.is_power_of_two());
@@ -16,6 +18,16 @@ fn make_shift(x: usize) -> usize {
 
 fn make_buckets<K, V>(x: usize) -> Box<[Atomic<Element<K, V>>]> {
     iter::repeat(Atomic::null()).take(x).collect()
+}
+
+fn hash2idx(hash: u64, shift: usize) -> usize {
+    hash as usize >> shift
+}
+
+fn do_hash(f: &impl BuildHasher, i: &(impl ?Sized + Hash)) -> u64 {
+    let mut hasher = f.build_hasher();
+    i.hash(&mut hasher);
+    hasher.finish()
 }
 
 pub struct BucketArray<K, V, S> {
@@ -39,5 +51,63 @@ impl<K: Eq + Hash, V, S: BuildHasher> BucketArray<K, V, S> {
             buckets,
             next: Atomic::null(),
         }
+    }
+
+    fn get_next<'a>(&self, guard: &'a Guard) -> Option<&'a Self> {
+        unsafe { self.next.load(Ordering::SeqCst, guard).as_ref() }
+    }
+
+    fn get_elem<'a, Q>(&'a self, guard: &'a Guard, key: &Q) -> Option<&'a Element<K, V>>
+    where
+        K: Borrow<Q>,
+        Q: ?Sized + Eq + Hash,
+    {
+        let hash = do_hash(&*self.hash_builder, key);
+        let mut idx = hash2idx(hash, self.shift);
+
+        loop {
+            let shared = unsafe { self.buckets[idx].load(Ordering::SeqCst, guard) };
+            match shared.tag() {
+                REDIRECT_TAG => {
+                    return self.get_next(guard).unwrap().get_elem(guard, key);
+                }
+
+                TOMBSTONE_TAG => {
+                    idx = hash2idx(idx as u64 + 1, self.shift);
+                    continue;
+                }
+
+                _ => (),
+            }
+            if shared.is_null() {
+                return None;
+            }
+            let elem = unsafe { shared.as_ref().unwrap() };
+            if hash == elem.hash && key == elem.key.borrow() {
+                return Some(elem);
+            } else {
+                idx = hash2idx(idx as u64 + 1, self.shift);
+            }
+        }
+    }
+
+    pub fn get<'a, Q>(&'a self, guard: &'a Guard, key: &Q) -> Option<ElementReadGuard<'a, K, V>>
+    where
+        K: Borrow<Q>,
+        Q: ?Sized + Eq + Hash,
+    {
+        self.get_elem(guard, key).map(|e| e.read(pin()))
+    }
+
+    pub fn get_mut<'a, Q>(
+        &'a self,
+        guard: &'a Guard,
+        key: &Q,
+    ) -> Option<ElementWriteGuard<'a, K, V>>
+    where
+        K: Borrow<Q>,
+        Q: ?Sized + Eq + Hash,
+    {
+        self.get_elem(guard, key).map(|e| e.write(pin()))
     }
 }
