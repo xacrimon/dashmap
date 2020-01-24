ec16351b9e3b07675c0d3e56460d79c7f975dc87
Acrimon <joel.wejdenstal@gmail.com>
2020-01-24 07:30:40 +0100

    Started working on core table structure.

--- src/element.rs
+++ src/element.rs
@@ -1,36 +1,41 @@
 use spin::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 use std::cell::UnsafeCell;
 use std::ops::{Deref, DerefMut};
+use crossbeam_epoch::Guard;
 
 pub struct Element<K, V> {
     pub lock: RwLock<()>,
     pub key: K,
+    pub hash: u64,
     pub value: UnsafeCell<V>,
 }
 
 impl<K, V> Element<K, V> {
-    pub fn new(key: K, value: V) -> Self {
+    pub fn new(key: K, hash: u64, value: V) -> Self {
         Self {
             lock: RwLock::new(()),
             key,
+            hash,
             value: UnsafeCell::new(value),
         }
     }
 
-    pub fn read<'a>(&'a self) -> ElementReadGuard<'a, K, V> {
+    pub fn read<'a>(&'a self, guard: Guard) -> ElementReadGuard<'a, K, V> {
         unsafe {
             ElementReadGuard {
-                _guard: self.lock.read(),
+                _lock_guard: self.lock.read(),
+                _mem_guard: guard,
                 key: &self.key,
                 value: &*self.value.get(),
             }
         }
     }
 
-    pub fn write<'a>(&'a self) -> ElementWriteGuard<'a, K, V> {
+    pub fn write<'a>(&'a self, guard: Guard) -> ElementWriteGuard<'a, K, V> {
         unsafe {
             ElementWriteGuard {
-                _guard: self.lock.write(),
+                _lock_guard: self.lock.write(),
+                _mem_guard: guard,
                 key: &self.key,
                 value: &mut *self.value.get(),
             }
@@ -39,7 +44,8 @@ impl<K, V> Element<K, V> {
 }
 
 pub struct ElementReadGuard<'a, K, V> {
-    pub _guard: RwLockReadGuard<'a, ()>,
+    pub _lock_guard: RwLockReadGuard<'a, ()>,
+    pub _mem_guard: Guard,
     pub key: &'a K,
     pub value: &'a V,
 }
@@ -53,7 +59,8 @@ impl<'a, K, V> Deref for ElementReadGuard<'a, K, V> {
 }
 
 pub struct ElementWriteGuard<'a, K, V> {
-    pub _guard: RwLockWriteGuard<'a, ()>,
+    pub _lock_guard: RwLockWriteGuard<'a, ()>,
+    pub _mem_guard: Guard,
     pub key: &'a K,
     pub value: &'a mut V,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 #![allow(dead_code)]
 
 pub mod element;
+pub mod table;
