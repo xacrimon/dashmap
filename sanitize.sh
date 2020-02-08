7aeb9a1ba594b5473da800ea8e1a08ff463f05df
Acrimon <joel.wejdenstal@gmail.com>
2020-02-08 11:16:37 +0100

    sanitize script improvements.

--- sanitize.sh
+++ sanitize.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-RUSTFLAGS="-Zsanitizer=${1}" cargo +nightly test -- --nocapture
+RUSTFLAGS="-Zsanitizer=memory" cargo +nightly test -- --nocapture
+RUSTFLAGS="-Zsanitizer=thread" cargo +nightly test -- --nocapture
