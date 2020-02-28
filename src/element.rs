4cc62542790bbda2b69e1a2380fae9956b6ecd1e
Acrimon <joel.wejdenstal@gmail.com>
2020-02-28 10:26:43 +0100

    Refactored element.rs to use the new refcounting system.

--- src/element.rs
+++ src/element.rs
@@ -1,4 +1,4 @@
-use crate::alloc::Sarc;
+use crate::alloc::{ABox, sarc_remove_copy, sarc_add_copy, sarc_deref};
 use crate::util::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 use std::cell::UnsafeCell;
 use std::mem::transmute;
@@ -21,28 +21,32 @@ impl<K, V> Element<K, V> {
         }
     }
 
-    pub fn read(data: Sarc<Element<K, V>>) -> ElementReadGuard<K, V> {
+    pub fn read(ptr: *const ABox<Element<K, V>>) -> ElementReadGuard<K, V> {
         unsafe {
+            sarc_add_copy(ptr);
+            let data = sarc_deref(ptr);
             let _lock_guard = transmute(data.lock.read());
             let key = transmute(&data.key);
             let value = transmute(&data.value);
             ElementReadGuard {
                 _lock_guard,
-                _mem_guard: data,
+                mem_guard: ptr,
                 key,
                 value,
             }
         }
     }
 
-    pub fn write(data: Sarc<Element<K, V>>) -> ElementWriteGuard<K, V> {
+    pub fn write(ptr: *const ABox<Element<K, V>>) -> ElementWriteGuard<K, V> {
         unsafe {
+            sarc_add_copy(ptr);
+            let data = sarc_deref(ptr);
             let _lock_guard = transmute(data.lock.write());
             let key = transmute(&data.key);
             let value = transmute(&data.value);
             ElementWriteGuard {
                 _lock_guard,
-                _mem_guard: data,
+                mem_guard: ptr,
                 key,
                 value,
             }
@@ -52,9 +56,17 @@ impl<K, V> Element<K, V> {
 
 pub struct ElementReadGuard<K, V> {
     _lock_guard: RwLockReadGuard<'static, ()>,
-    _mem_guard: Sarc<Element<K, V>>,
+    mem_guard: *const ABox<Element<K, V>>,
     key: *const K,
     value: *const V,
+}
+
+impl<K, V> Drop for ElementReadGuard<K, V> {
+    fn drop(&mut self) {
+        unsafe {
+            sarc_remove_copy(self.mem_guard);
+        }
+    }
 }
 
 impl<K, V> ElementReadGuard<K, V> {
@@ -81,9 +93,17 @@ impl<K, V> Deref for ElementReadGuard<K, V> {
 
 pub struct ElementWriteGuard<K, V> {
     _lock_guard: RwLockWriteGuard<'static, ()>,
-    _mem_guard: Sarc<Element<K, V>>,
+    mem_guard: *const ABox<Element<K, V>>,
     key: *const K,
     value: *mut V,
+}
+
+impl<K, V> Drop for ElementWriteGuard<K, V> {
+    fn drop(&mut self) {
+        unsafe {
+            sarc_remove_copy(self.mem_guard);
+        }
+    }
 }
 
 impl<K, V> ElementWriteGuard<K, V> {
