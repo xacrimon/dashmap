dced94a1abe335e4554fc2f38bbf2bb033c6838d
Acrimon <joel.wejdenstal@gmail.com>
2020-02-28 01:40:19 +0100

    Started reworking the GC. *

--- src/recl.rs
+++ src/recl.rs
@@ -1,110 +1,41 @@
-//! Simple QSBR garbage collector.
-//! This probably isn't the optimal model for memory reclamation. Stamp-it, Hazard Eras and EBR should be considered.
+//! Simple EBR garbage collector.
+//! TO-DO: Research Stamp-it and Debra, optimize this garbage collector.
+//! https://arxiv.org/abs/1805.08639
+//! https://arxiv.org/pdf/1712.01044.pdf
 
-use std::sync::{Mutex, MutexGuard, Arc};
-use std::mem::{swap, take};
-use std::thread::{ThreadId, current};
-use std::collections::HashMap;
-
-type GarbageList = Vec<Box<dyn FnOnce()>>;
+use std::sync::{Mutex, Arc};
+use std::thread::{self, ThreadId};
 
 fn id() -> ThreadId {
-    current().id()
+    thread::current().id()
 }
 
-fn next_era<'a>(mut state: MutexGuard<'a, GcState>) -> GarbageList {
-    let state = &mut *state;
-    let previous_interval = take(&mut state.previous_interval);
-    
-    swap(&mut state.previous_interval, &mut state.current_interval);
-
-    state.thread_was_quiescent
-        .iter_mut()
-        .for_each(|(_, flag)| *flag = false);
-        
-    state.num_remaining = state.thread_was_quiescent.len();
-    previous_interval
+struct Global {
+    state: Mutex<GlobalState>,
 }
 
-pub struct Gc {
-    state: Arc<Mutex<GcState>>,
+struct GlobalState {
+    // Global epoch. This value is always 0, 1 or 2.
+    epoch: usize,
+
+    // Deferred functions.
+    deferred: Vec<Box<dyn FnOnce()>>,
+
+    // List of participants.
+    locals: Vec<*const Local>,
 }
 
-struct GcState {
-    thread_was_quiescent: HashMap<ThreadId, bool>,
-    current_interval: GarbageList,
-    previous_interval: GarbageList,
-    num_remaining: usize,
+struct Local {
+    state: Mutex<LocalState>,
 }
 
-impl Gc {
-    pub fn new() -> Self {
-        let state = GcState {
-            thread_was_quiescent: HashMap::new(),
-            current_interval: Vec::new(),
-            previous_interval: Vec::new(),
-            num_remaining: 0,
-        };
+struct LocalState {
+    // Active flag.
+    active: bool,
 
-        Self {
-            state: Arc::new(Mutex::new(state)),
-        }
-    }
+    // Local epoch. This value is always 0, 1 or 2.
+    epoch: usize,
 
-    pub fn defer(&self, f: impl FnOnce() + 'static) {
-        let mut state = self.state.lock().unwrap();
-        let f = Box::new(f);
-        state.current_interval.push(f);
-    }
-
-    pub fn register_thread(&self) {
-        let mut state = self.state.lock().unwrap();
-        let id = id();
-        state.thread_was_quiescent.insert(id, false);
-        state.num_remaining += 1;
-    }
-
-    pub fn unregister_thread(&self) {
-        let mut state = self.state.lock().unwrap();
-        let id = id();
-        state.thread_was_quiescent.remove(&id);
-        state.num_remaining -= 1;
-        
-        if state.num_remaining == 0 {
-            next_era(state)
-                .into_iter()
-                .for_each(|callback| callback());
-        }
-    }
-
-    pub fn on_quiescent_state(&self) {
-        let mut state = self.state.lock().unwrap();
-        let id = id();
-        let flag = state.thread_was_quiescent.get_mut(&id).unwrap();
-
-        if !*flag {
-            *flag = true;
-            state.num_remaining -= 1;
-
-            if state.num_remaining == 0 {
-                next_era(state)
-                    .into_iter()
-                    .for_each(|callback| callback());
-            }
-        }
-    }
-}
-
-impl Drop for Gc {
-    fn drop(&mut self) {
-        let mut state = self.state.lock().unwrap();
-        
-        take(&mut state.previous_interval)
-            .into_iter()
-            .for_each(|callback| callback());
-
-        take(&mut state.current_interval)
-            .into_iter()
-            .for_each(|callback| callback());
-    }
+    // Reference to global state.
+    global: Arc<Global>,
 }
