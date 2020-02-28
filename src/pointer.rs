2caab0cc74f08e0b48bac7796d49889d873062e2
Acrimon <joel.wejdenstal@gmail.com>
2020-02-28 11:30:16 +0100

    Started reworking table. Save commit.

---
+++ src/pointer.rs
@@ -0,0 +1,24 @@
+use std::mem;
+
+fn low_bits<T>() -> usize {
+    (1 << mem::align_of::<T>().trailing_zeros()) - 1
+}
+
+fn data_with_tag<T>(data: usize, tag: usize) -> usize {
+    (data & !low_bits::<T>()) | (tag & low_bits::<T>())
+}
+
+fn decompose_data<T>(data: usize) -> (*mut T, usize) {
+    let raw = (data & !low_bits::<T>()) as *mut T;
+    let tag = data & low_bits::<T>();
+    (raw, tag)
+}
+
+pub fn p_tag<T>(p: *const T) -> usize {
+    let (_, tag) = decompose_data::<T>(p);
+    tag
+}
+
+pub fn p_set_tag<T>(p: *const T, tag: usize) -> *mut T {
+    data_with_tag(p as usize, tag) as _
+}
