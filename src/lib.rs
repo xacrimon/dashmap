94d89c461d2c5bdc184564ee090068357e390684
Acrimon <joel.wejdenstal@gmail.com>
2020-03-10 13:19:16 +0100

    Implemented extract on table.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
-#![cfg_attr(
-    feature = "nightly",
-    feature(core_intrinsics),
-)]
+#![cfg_attr(feature = "nightly", feature(core_intrinsics))]
 
 mod alloc;
 mod element;
