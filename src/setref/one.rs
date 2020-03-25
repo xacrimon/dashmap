a3e87ddd5a8095d4775d1cc376cecde75d81282b
TyPR124 <t.ruckinger@gmail.com>
2020-03-25 16:39:42 -0400

    some cleanup

--- src/setref/one.rs
+++ src/setref/one.rs
@@ -31,27 +31,3 @@ impl<'a, K: Eq + Hash, S: BuildHasher> Deref for Ref<'a, K, S> {
         self.key()
     }
 }
-
-
-// No need for RefMut - cannot mutate key in hashmap anyway
-// pub struct RefMut<'a, K, S = RandomState> {
-//     inner: mapref::one::RefMut<'a, K, (), S>
-// }
-
-// unsafe impl<'a, K: Eq + Hash + Send, S: BuildHasher> Send for RefMut<'a, K, S> {}
-// unsafe impl<'a, K: Eq + Hash + Send + Sync, S: BuildHasher> Sync
-//     for RefMut<'a, K, S>
-// {
-// }
-
-// impl<'a, K: Eq + Hash, S: BuildHasher> Ref<'a, K, S> {
-//     #[inline(always)]
-//     pub(crate) fn new(inner: mapref::one::RefMut<'a, K, (), S>) -> Self {
-//         Self { inner }
-//     }
-//     #[inline(always)]
-//     pub fn key(&self) -> &K {
-//         self.inner.key()
-//     }
-// }
-
