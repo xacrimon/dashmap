8cac25dc1569aa58c35c0eee5818581d32606c49
Acrimon <joel.wejdenstal@gmail.com>
2020-04-04 00:54:14 +0200

    cleanup.

--- src/mvcc.rs
+++ src/mvcc.rs
@@ -5,20 +5,23 @@ use std::sync::atomic::{Ordering, AtomicUsize};
 use std::sync::Mutex;
 use std::hash::{Hash, BuildHasher};
 
+type TxId = usize;
+type AtomicTxId = AtomicUsize;
+
 struct Record<V> {
     data: V,
-    created: AtomicUsize,
-    expired: AtomicUsize,
+    created: AtomicTxId,
+    expired: AtomicTxId,
     next: Mutex<Option<*const Self>>,
-}
-
-#[derive(Hash)]
-struct CompositeKey<K> {
-    id: usize,
-    key: K,
 }
 
 pub struct MVCCTable<K, V, S> {
     next_txid: AtomicUsize,
-    table: Table<CompositeKey<K>, Record<V>, S>,
+    active: Table<TxId, (), S>,
+    records: Table<K, Record<V>, S>,
+}
+
+pub struct Transaction<'a, K, V, S> {
+    id: TxId,
+    table: &'a MVCCTable<K, V, S>,
 }
