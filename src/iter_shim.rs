7fe7aa05f457d2a095a9a00bdc8af51c0d08c129
Acrimon <joel.wejdenstal@gmail.com>
2020-04-18 02:45:38 +0200

    Minor fixes to iterator shim.

--- src/iter_shim.rs
+++ src/iter_shim.rs
@@ -1,17 +1,17 @@
 use crate::ElementGuard;
 use std::hash::Hash;
 
-pub struct Iter<'a, K, V> {
-    inner: Box<dyn Iterator<Item = ElementGuard<K, V>> + Send + Sync + 'a>,
+pub struct Iter<K, V> {
+    inner: Box<dyn Iterator<Item = ElementGuard<K, V>> + Send + Sync>,
 }
 
-impl<'a, K: Eq + Hash, V> Iter<'a, K, V> {
-    pub fn new(inner: Box<dyn Iterator<Item = ElementGuard<K, V>> + Send + Sync + 'a>) -> Self {
+impl<K: Eq + Hash, V> Iter<K, V> {
+    pub fn new(inner: Box<dyn Iterator<Item = ElementGuard<K, V>> + Send + Sync>) -> Self {
         Self { inner }
     }
 }
 
-impl<'a, K: Eq + Hash, V> Iterator for Iter<'a, K, V> {
+impl<K: Eq + Hash, V> Iterator for Iter<K, V> {
     type Item = ElementGuard<K, V>;
 
     fn next(&mut self) -> Option<Self::Item> {
