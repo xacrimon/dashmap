d9ceda6ab977a4176d13f96b603c606afec59d08
Acrimon <joel.wejdenstal@gmail.com>
2020-04-18 21:36:47 +0200

    Fix capacity miscalculation.

--- src/spec/mod.rs
+++ src/spec/mod.rs
@@ -1,7 +1,9 @@
-//#[cfg(target_arch = "x86_64")]
-//pub mod x86_64;
-//pub use x86_64::Table;
+#[cfg(target_arch = "x86_64")]
+pub mod x86_64;
+#[cfg(target_arch = "x86_64")]
+pub use x86_64::Table;
 
-//#[cfg(not(target_arch = "x86_64"))]
+#[cfg(not(target_arch = "x86_64"))]
 pub mod fallback;
+#[cfg(not(target_arch = "x86_64"))]
 pub use fallback::Table;
