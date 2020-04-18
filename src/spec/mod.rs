0740faf4aaefa0f7c5920ca1446ff99248712952
Acrimon <joel.wejdenstal@gmail.com>
2020-04-18 11:48:46 +0200

    Started implementing fallback backend.

--- src/spec/mod.rs
+++ src/spec/mod.rs
@@ -1,3 +1,7 @@
-#[cfg(target_arch = "x86_64")]
-pub mod x86_64;
-pub use x86_64::Table;
+//#[cfg(target_arch = "x86_64")]
+//pub mod x86_64;
+//pub use x86_64::Table;
+
+//#[cfg(not(target_arch = "x86_64"))]
+pub mod fallback;
+pub use fallback::Table;
