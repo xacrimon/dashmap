6610b3829f404d292b402af917152c0a0f4deeb5
Acrimon <joel.wejdenstal@gmail.com>
2020-06-10 01:34:23 +0200

    Turned on some lints and fixed crate docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
 #![allow(unused_unsafe)]
+#![warn(missing_docs)]
+#![deny(clippy::correctness)]
+#![warn(clippy::style)]
+#![warn(clippy::complexity)]
+#![warn(clippy::perf)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::cargo)]
 
-///! `dashmap` provides a relatively low level high performance concurrent hash map.
-///! See struct level docs for more details.
-///!
-///! Serde is supported if the `serde` feature is enabled.
-///! Then `DashMap` will implement `Serialize` and `Deserialize`.
+//! `dashmap` provides a relatively low level high performance concurrent hash map.
+//! See struct level docs for more details.
+//!
+//! Serde is supported if the `serde` feature is enabled.
+//! Then `DashMap` will implement `Serialize` and `Deserialize`.
+
 mod alloc;
 mod element;
 mod iter_shim;
