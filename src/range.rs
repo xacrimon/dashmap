717aa24767faa7aa4cbee6ea923a3ab83a841b12
Acrimon <joel.wejdenstal@gmail.com>
2020-08-05 10:54:03 +0200

    test: make sure the circular iterator yields the expected set of values for a specific range

--- src/range.rs
+++ src/range.rs
@@ -2,14 +2,16 @@ pub struct CircularRange {
     wrap: usize,
     stop: usize,
     next: usize,
+    first: bool,
 }
 
 impl CircularRange {
     pub fn new(start: usize, wrap: usize) -> Self {
         Self {
             wrap,
-            stop: start,
+            stop: start + 1,
             next: start,
+            first: true,
         }
     }
 }
@@ -23,10 +25,24 @@ impl Iterator for CircularRange {
 
         if self.next == self.wrap {
             self.next = 0;
-        } else if self.next == self.stop {
+        } else if self.next == self.stop && !self.first {
             return None;
         }
 
+        self.first = false;
         Some(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::CircularRange;
+
+    #[test]
+    fn check_set() {
+        let set: Vec<usize> = CircularRange::new(5, 10).collect();
+        let expected = [5, 6, 7, 8, 9, 0, 1, 2, 3, 4];
+
+        assert_eq!(&set, &expected);
     }
 }
