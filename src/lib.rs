0408162e5b1ac54bc059c649eceb6a2937efa11b
Alisa Sireneva <me@purplesyringa.moe>
2025-12-28 19:44:10 +0300

    Replace map_or with is_some_and

--- src/lib.rs
+++ src/lib.rs
@@ -1333,11 +1333,9 @@ where
 impl<K: Eq + Hash, V: PartialEq, S: BuildHasher + Clone> PartialEq for DashMap<K, V, S> {
     fn eq(&self, other: &Self) -> bool {
         self.len() == other.len()
-            && self.iter().all(|r| {
-                other
-                    .get(r.key())
-                    .map_or(false, |ro| r.value() == ro.value())
-            })
+            && self
+                .iter()
+                .all(|r| other.get(r.key()).is_some_and(|ro| r.value() == ro.value()))
     }
 }
 
